d55849a01350465946e2eb85d5464d55c4744797
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2021-11-07 07:00:12 +0700

    Create kalkulator-sederhana.py

---
+++ kalkulator-sederhana.py
@@ -0,0 +1,18 @@
+while True: #mengulang operasi kalo udah selesai
+    print("\nKeterangan :\n+ = Pertamabahan\n- = Pengurangan\n* atau × = Perkalian\n/ atau : = Pembagian\n")
+    operasi = input("Tentukan operasi yang ingin Anda lakukan,\nmisalnya +, -, *, / : ") 
+    if operasi not in("+", "-", "*", "×", ":", "/"):
+        print("Maaf operasi yang Anda masukan salah!") #kalo ngemasukin selain dari operasi yg diatas.
+        break #buat berhentiin
+        
+    x = int(input("Masukkan angka pertama : ")) #masukin angka 1
+    y = int(input("Masukkan angka pertama : ")) #masukin angka 2
+    
+    if operasi == "+": #jika ditambah
+        print("Hasil dari", x, "+", y, "=", x+y)
+    elif operasi == "-": #jika dikurang
+        print("Hasil dari", x, "-", y, "=", x-y)
+    elif operasi == "*" or operasi == "×": #jika dikali
+        print("Hasil dari", x, "×", y, "=", x*y)
+    elif operasi == "/" or operasi == ":": #jika dibagi
+        print("Hasil dari", x, ":", y, "=", x/y)
