1fc1d8fcee39f957ca3e594ab294e8d0434064ab
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2021-11-10 06:47:55 +0700

    lebih simple

--- harga-baju.py
+++ harga-baju.py
@@ -1,28 +1,36 @@
-merk = input("Merk baju P/A/S : ")
-if merk == 'P'or merk == 'p':
+merk = input("Merk baju P/A/S : ").lower()
+if merk == 'p':
   print ('Merk Polo')
-  ukuran = input('ukuran L/M/S : ' )
-  if ukuran == 'L' or ukuran == 'l':
+  ukuran = input('ukuran L/M/S : ' ).lower()
+  if ukuran == 'l':
     print('Harga = 300000')
-  elif ukuran == 'M' or ukuran == 'm':
+  elif ukuran == 'm':
     print('Harga = 225000')
-  else:
+  elif ukuran == 's':
     print('Harga = 175000')
-elif merk == 'A' or merk == 'a':
+  else:
+    print('Maaf pilihan ukuran tidak tersedia!')
+elif merk == 'a':
   print('Merk Alisan')
-  ukuran = input('Ukuran L/M/S : ')
-  if ukuran == 'L' or ukuran == 'l':
+  ukuran = input('Ukuran L/M/S : ').lower()
+  if ukuran == 'l':
     print('Harga = 275000')
-  elif ukuran == 'M' or ukuran == 'm':
+  elif ukuran == 'm':
     print('Harga = 200000')
-  else:
+  elif ukuran == 's':
     print('Harga = 150000')
-else:
-  print('Merk StYess')
-  ukuran = input('Ukuran L/M/S : ')
-  if ukuran == 'L' or ukuran == 'l':
-    print('Harga = 250000')
-  elif ukuran == 'M' or ukuran == 'm':
-    print('Harga = 175000')
   else:
+    print('Maaf ukuran tidak tersedia!')
+elif merk == 's':
+  print('Merk StYess')
+  ukuran = input('Ukuran L/M/S : ').lower()
+  if ukuran == 'l':
+    print('Harga = 250000')
+  elif ukuran == 'm':
+    print('Harga = 175000')
+  elif ukuran == 's':
     print('Harga = 125000')
+  else:
+    print('Maaf ukuran tidak tersedia!')
+else:
+    print('Maaf merk tidak tersedia!')
