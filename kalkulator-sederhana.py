05c70e2e78131b557e85711ba95981053217d525
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2021-11-16 20:12:45 +0700

    Rapihin dikit
    Penambahan judul, rapihin dikit beberapa, dan penambahan fungsi untuk mengulang jika yg diinput bukan angka!

--- kalkulator-sederhana.py
+++ kalkulator-sederhana.py
@@ -1,18 +1,28 @@
-while True: #mengulang operasi kalo udah selesai
-    print("\nKeterangan :\n+ = Pertamabahan\n- = Pengurangan\n* atau × = Perkalian\n/ atau : = Pembagian\n")
-    operasi = input("Tentukan operasi yang ingin Anda lakukan,\nmisalnya +, -, *, / : ") 
-    if operasi not in("+", "-", "*", "×", ":", "/"):
-        print("Maaf operasi yang Anda masukan salah!") #kalo ngemasukin selain dari operasi yg diatas.
-        break #buat berhentiin program atau ubah break menjadi continue buat skip code yg dibawah dan ngulang program dari awal lagi jika operasi yang dimasukkan salah
-        
-    x = int(input("Masukkan angka pertama : ")) #masukin angka ke-1
-    y = int(input("Masukkan angka kedua : ")) #masukin angka ke-2
-    
-    if operasi == "+": #jika ditambah
-        print("Hasil dari", x, "+", y, "=", x+y)
-    elif operasi == "-": #jika dikurang
-        print("Hasil dari", x, "-", y, "=", x-y)
-    elif operasi == "*" or operasi == "×": #jika dikali
-        print("Hasil dari", x, "×", y, "=", x*y)
-    elif operasi == "/" or operasi == ":": #jika dibagi
-        print("Hasil dari", x, ":", y, "=", x/y)
+def start():
+	while True:
+		judul = "Kalkulator Sederhana".center(50)
+		print(judul)
+		print('='*50)
+		print("\nKeterangan :\n+ = Pertamabahan\n- = Pengurangan\n* atau × = Perkalian\n/ atau : = Pembagian\n")
+		operasi = input("Tentukan operasi yang ingin Anda lakukan,\nmisalnya +, -, *, / : ") 
+		if operasi not in("+", "-", "*", "×", ":", "/"):
+			print("\nMaaf operasi yang Anda masukan salah!, silahkan coba lagi!\n")#kalo ngemasukin selain dari operasi yg diatas.
+			
+			break #buat berhentiin program atau ubah break menjadi continue atau juga bisa pakai start() buat skip code yg dibawah dan ngulang program dari awal lagi jika operasi yang dimasukkan salah
+		
+		try:
+				x = int(input("\nMasukkan angka pertama : ")) #masukin angka ke-
+				y = int(input("Masukkan angka kedua : ")) #masukin angka ke-2
+		except:
+			    print("\nMaaf yang Anda masukkan bukan angka, silahkan coba lagi!\n")
+			    break #fungsinya sama kek di line 9
+		if operasi == "+": #jika ditambah
+			print("Hasil dari", x, "+", y, "=", x+y)
+		elif operasi == "-": #jika dikurang
+		    print("Hasil dari", x, "-", y, "=", x-y)
+		elif operasi == "*" or operasi == "×": #jika dikali
+		    print("Hasil dari", x, "×", y, "=", x*y)
+		elif operasi == "/" or operasi == ":": #jika dibagi
+		    print("Hasil dari", x, ":", y, "=", x/y)
+	
+start()
